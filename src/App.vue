84bed41e08916d625804cad78de5c721c4686725
Slashinkun <ktang3214@gmail.com>
2025-04-29 13:36:06 +0200

    fixing bug, adding : budget, localstorage, user's categories, delete user's expense

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <script setup>
 import { ref, computed } from 'vue'
-import Category from './components/Category.vue'
 import ExpenseCard from './components/ExpenseCard.vue'
 
-const categories = ref([])
+const localStorageUserCategory = localStorage.getItem('user_categories')
+const categories = ref(
+  localStorageUserCategory
+    ? JSON.parse(localStorageUserCategory)
+    : ['Health', 'Food', 'Travel', 'House', 'Leisure'],
+)
 const newCategory = ref('')
 
-const expenses = ref([])
+const remainingBudget = computed(() => {
+  let sumExpenses = expenses.value.reduce((acc, expense) => acc + expense.amount, 0)
+
+  return userBudget.value - sumExpenses
+})
+
+const localStorageExpenses = localStorage.getItem('user_expenses')
+const expenses = ref(localStorageExpenses ? JSON.parse(localStorageExpenses) : [])
+
+const newUserBudget = ref(0)
+const userBudget = ref(
+  localStorage.getItem('user_budget') ? JSON.parse(localStorage.getItem('user_budget')) : 0,
+)
+const validateBudget = () => {
+  if (newUserBudget.value > 0) {
+    localStorage.setItem('user_budget', newUserBudget.value)
+    newUserBudget.value = 0
+  }
+}
 
 const newExpense = ref({
+  id: expenses.value.length + 1,
   amount: 0,
   category: '',
   description: '',
@@ -20,52 +43,118 @@ const addCategories = () => {
     return
   } else {
     categories.value.push(newCategory.value)
+    localStorage.setItem('user_categories', JSON.stringify(categories.value))
+    newCategory.value = ''
   }
 }
 
 const addExpense = () => {
   console.log(newExpense.value)
 
-  if (newExpense.value.amount === 0) {
-    return
-  }
-  if (newExpense.value.date === null) {
+  if (!newExpense.value.amount || !newExpense.value.date) {
     return
   }
 
   expenses.value.push({ ...newExpense.value })
   console.log(expenses)
 
-  newExpense.value.amount = 0
-  newExpense.value.date = ''
-  newExpense.value.category = ''
-  newExpense.value.description = ''
+  localStorage.setItem('user_expenses', JSON.stringify(expenses.value))
+
+  newExpense.value = {
+    amount: 0,
+    date: '',
+    category: '',
+    description: '',
+  }
+}
+
+const deleteExpense = (id) => {
+  const index = expenses.value.indexOf(id)
+  expenses.value.splice(index, 1)
+  localStorage.setItem('user_expenses', JSON.stringify(expenses.value))
 }
 </script>
 
 <template>
-  <input type="text" v-model.trim="newCategory" @keyup.enter="addCategories" class="" />
-  <div v-for="cat in categories">
-    <Category :title="cat"></Category>
+  <h1>Budget App</h1>
+  <h2>
+    Your budget : <span>{{ userBudget }} $</span>
+  </h2>
+  <h3>
+    Remaining :
+    <span
+      v-bind:class="{
+        'negative-budget': remainingBudget < 0,
+        'positive-budget': remainingBudget > userBudget * 0.75,
+        'average-budget': remainingBudget < userBudget * 0.75,
+      }"
+      >{{ remainingBudget }} $
+    </span>
+  </h3>
+
+  <div class="container">
+    <h2>Add a category</h2>
+    <div><input type="text" v-model.trim="newCategory" @keyup.enter="addCategories" /></div>
+    <input type="number" placeholder="Enter your budget" v-model="newUserBudget" />
+    <button class="btn btn-success" @click="validateBudget">Validate</button>
   </div>
+
   <div>
-    <form>
-      <label for="amount"> Amount</label>
-      <input type="number" min="1" v-model="newExpense.amount" />
-      <label for="description">Description</label>
-      <textarea v-model="newExpense.description"></textarea>
-      <label for="categories">Category</label>
-      <select name="" id="">
-        <option v-for="cat in categories" :value="cat">{{ cat }}</option>
+    <h2>Add an expense</h2>
+    <form class="expense-form" @submit.prevent="addExpense" id="expense-form">
+      <label for="expense_amount"> Amount</label>
+      <input
+        type="number"
+        id="expense_amount"
+        min="1"
+        v-model.number="newExpense.amount"
+        required
+      />
+      <label for="expense_desc">Description</label>
+      <input v-model="newExpense.description" id="expense_desc" placeholder="For what ?" />
+      <label for="expense_category">Category</label>
+      <select v-model="newExpense.category" id="expense_category">
+        <option value=""></option>
+        <option v-for="cat in categories" :key="cat" :value="cat">{{ cat }}</option>
       </select>
-      <label for="date">Date</label>
-      <input type="date" v-model="newExpense.date" />
-      <button type="submit" class="btn btn-primary" @click.prevent="addExpense">Add</button>
+      <label for="expense_date">Date</label>
+      <input type="date" id="expense_date" v-model="newExpense.date" required />
+      <button
+        type="submit"
+        class="btn btn-primary"
+        v-bind:disabled="newExpense.amount === 0 || newExpense.amount === ''"
+      >
+        Add
+      </button>
     </form>
   </div>
   <div v-for="expense in expenses">
-    <ExpenseCard :expense="expense"></ExpenseCard>
+    <ExpenseCard
+      :expense="expense"
+      @delete="deleteExpense(expense.id)"
+      :key="expense.id"
+    ></ExpenseCard>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.expense-form {
+  margin: auto;
+}
+
+.expense-form::after {
+  margin: 4px;
+}
+
+.negative-budget {
+  color: red;
+}
+
+.positive-budget {
+  color: green;
+}
+
+.average-budget {
+  color: orange;
+}
+</style>
--- src/components/ExpenseCard.vue
+++ src/components/ExpenseCard.vue
@@ -1,12 +1,38 @@
 <script setup>
 defineProps({ expense: Object })
+const emit = defineEmits(['delete'])
 </script>
 
 <template>
-  <h3>{{ expense.amount }}</h3>
-  <p>{{ expense.description }}</p>
-  <p>{{ expense.category }}</p>
-  <p>{{ expense.date }}</p>
+  <div class="container">
+    <div class="category-description">
+      <div>{{ expense.category }}</div>
+      <div>{{ expense.description }}</div>
+      <div>{{ expense.date }}</div>
+    </div>
+    <div class="actions">
+      <div class="amount">{{ expense.amount }} $</div>
+      <button class="btn btn-danger" @click="$emit('delete')">Delete</button>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: flex-start;
+  border: 1px solid;
+  margin: 10px auto;
+}
+
+.category-description {
+  display: flex;
+  flex-direction: column;
+}
+
+.actions {
+  display: flex;
+}
+</style>
